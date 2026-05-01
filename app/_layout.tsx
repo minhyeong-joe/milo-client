d9397ec0e6728065c86d207af4a3591cedf7068a
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2026-04-30 20:44:40 -0700

    remove unnecessary fragment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
   );
 }
