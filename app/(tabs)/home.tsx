fcf50d5c0f47753e75049fc3a9032e08188be41b
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2026-04-30 23:25:05 -0700

    set up development build instead of expo go

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,14 +3,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { globalStyles } from "@/styles/globalStyles";
 
 export default function HomeScreen() {
-  return (
-    <SafeAreaView style={globalStyles.screen}>
-      <View style={globalStyles.screenContent}>
-        <Text style={globalStyles.titleText}>Home Screen</Text>
-        <Text style={globalStyles.bodyText}>
-          Main screen for logging sleep, meal, and diaper.
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={globalStyles.screen}>
+			<View style={globalStyles.screenContent}>
+				<Text style={globalStyles.titleText}>Home Screen</Text>
+				<Text style={globalStyles.bodyText}>
+					Main screen for logging sleep, meal, and diaper.
+				</Text>
+			</View>
+		</SafeAreaView>
+	);
 }
